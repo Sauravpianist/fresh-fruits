f4e361f1748b98fa1b5ab348fc95c4b6ef1ea5ae
Saurav <saurav.21scse1011071@galgotiasuniversity.edu.in>
2023-07-19 01:03:50 +0530

    fresh fruit updated

---
+++ js/btn.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('search');
+
+btn.addEventListener('click', function onClick() {
+  btn.style.borderColor ="none";
+  btn.style.boxShadow="1px 1px 2px #ff7200, 0 0 10px #ff7200, 0 0 5px #ff7200"
+});
